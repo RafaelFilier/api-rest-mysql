049ae59f1044059a3bee7a1165021f92a1f5ba37
RafaelFilier <rafafilierf@gmail.com>
2026-09-08 17:54:46 -0300

    continuando projeto

--- src/server.js
+++ src/server.js
@@ -6,22 +6,35 @@ import { pool } from "./db.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = Number(process.env.PORT) || 3002;
 
 app.use(cors());
 app.use(express.json());
 
 app.post("/", async (req, res) => {
     try {
-        const { name, email } = req.body;
+        const { nome, email } = req.body;
 
         const [result] = await pool.query(
-            "INSERT INTO usuarios VALUES (?, ?)",
-            [name, email]
+            "INSERT INTO usuarios (nome, email) VALUES (?, ?)",
+            [nome, email]
         );
 
-        res.status(201).json({ id: result.insertId, name, email });
+        res.status(201).json({ id: result.insertId, nome, email });
     } catch (e) {
-        res.status(500).json({ error: "Falha ao criar usuario" });
+        res.status(500).json({ erro: "Falha ao criar usuario" });
     }
+});
+
+app.get("/", async (req, res) => {
+    try {
+        const [rows] = await pool.query("SELECT * FROM usuarios");
+        res.json(rows);
+    } catch (e) {
+        res.status(500).json({ erro: "Falha ao listar usuarios"});
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
